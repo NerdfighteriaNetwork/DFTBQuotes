4ed161d141b9a0642a083e4c565f57adec71045d
Jason Spriggs <jason@piandcompany.com>
2012-02-19 13:12:55 -0500

    Example Config File

---
+++ example.config.php
@@ -0,0 +1,18 @@
+<?php
+/*
+config.php - DFTBQuotes configuration file
+Copyright 2012, Dimitri Molenaars (tyrope.nl)
+Licensed under the GNU General Public License
+
+More info:
+* https://github.com/elad661/DFTBQuotes
+*/
+
+//MySQL stuff
+$conf['sql_server'] = "localhost"; //MySQL server to connect to. (In most cases this is 'localhost')
+$conf['sql_user'] = ""; //MySQL username
+$conf['sql_pass'] = ""; //MySQL password
+$conf['sql_db'] = "dftbquotes"; //MySQL database
+$conf['sql_tbl_prefix'] = "dftbq_"; //Prefixes to add to the table names.
+
+?>
