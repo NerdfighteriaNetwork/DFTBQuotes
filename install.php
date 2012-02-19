e82229b3a952aa50fff3d010f28a0e1dee0cfa2a
Jason Spriggs <jason@piandcompany.com>
2012-02-19 14:23:42 -0500

    Documentation and Installer

---
+++ install.php
@@ -0,0 +1,28 @@
+<?php
+/*
+install.php - DFTBQuotes easy installer script
+Copyright (C) 2012 Jason Spriggs (jasonspriggs.com) <jason@jasonspriggs.com>
+
+    This file is part of DFTBQuotes.
+
+    DFTBQuotes is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DFTBQuotes is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with DFTBQuotes.  If not, see <http://www.gnu.org/licenses/>.
+
+install.php creates the table schema for DFTBQuotes and gathers basic information for the config.
+
+More info: https://github.com/elad661/DFTBQuotes 
+*/
+
+echo "Indev";
+
+?>
