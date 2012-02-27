72cd19d36de56668811821eb5b42ca3dc4545bec
Dimitri Molenaars <Tyrope@TyRope.nl>
2012-02-27 22:23:27 +0100

    Proper loading of pages, including errors.
    Signed-off-by: Dimitri Molenaars <Tyrope@TyRope.nl>

--- index.php
+++ index.php
@@ -18,21 +18,73 @@ Copyright (C) 2012 Dimitri Molenaars (tyrope.nl) <tyrope@tyrope.nl>
     along with DFTBQuotes.  If not, see <http://www.gnu.org/licenses/>.
 
 index.php loads in the active template, and decides which pages should be displayed.
+it also handles any errors that should be displayed, should they be displayed.
 
 More info: https://github.com/elad661/DFTBQuotes 
+ */
+
+/* Error codes:
+ 0 No error.
+ 1 Invalid Page ID.
+ 2 Authorization required.
 */
 
 require_once('inc/header.php');
 require_once('inc/menu.php');
+require_once('inc/connect.php');
 
-//This part will actually get the proper file from the database/an array.
-if(file_exists('themes/'.$conf['theme'].'/quotes.htm')){
-    $content = new TemplatePower('themes/'.$conf['theme'].'/quotes.htm');
+if(isset($_GET['pid'])){
+    //Load page ID, check if page is valid.
+    if(!is_int($_GET['pid'])){
+	$ERR_ID = 1;
+    }else{
+	//ask database if page exists
+	$result = mysql_query("SELECT page, folder, auth FROM ".$conf['sql_tbl_prefix']."pages WHERE pageID='".$_GET['pid']."' LIMIT 1");
+	if(mysql_num_rows($result) > 0){
+	    $data = mysql_fetch_assoc($result);
+	    if($data['auth']){
+		//authorization required.
+		if(!isset($_SESSION['loggedIn'])){
+		    //not logged in.
+		    $ERR_ID = 2;
+		}elseif($_SESSION['loggedIn'] < $data['auth']){
+		    //account not enough access.
+		    $ERR_ID = 2;
+		}else{
+		    //authorization accepted.
+		    if(file_exists('themes/'.$conf['theme'].'/quotes.htm')){
+			$content = new TemplatePower('themes/'.$conf['theme'].$data['folder'].'/'.$data['page']);
+		    }else{
+			$content = new TemplatePower('themes/default/'.$data['folder'].$data['page']);
+		    }
+		}
+	    }else{
+		//no authorization needed.
+		if(file_exists('themes/'.$conf['theme'].'/quotes.htm')){
+		    $content = new TemplatePower('themes/'.$conf['theme'].$data['folder'].'/'.$data['page']);
+		}else{
+		    $content = new TemplatePower('themes/default/'.$data['folder'].$data['page']);
+		}
+	    }
+	}
+    }
 }else{
-    $content = new TemplatePower('themes/default/quotes.htm');
-}    
+    // Load default page
+    if(file_exists('themes/'.$conf['theme'].'/quotes.htm')){
+	$content = new TemplatePower('themes/'.$conf['theme'].'/quotes.htm');
+    }else{
+	$content = new TemplatePower('themes/default/quotes.htm');
+    }
+}
+
+// Should an error be displayed?
+if($ERR_ID){
+    $content->newBlock("ERROR_".$ERR_ID);
+    $content->assign("PID",$_GET['pid']);
+    $content->prepare();
+}
 $content->prepare();
 
 require_once('inc/footer.php');
-
+mysql_close($link);
 ?>
